cb00cba3a5c10cb1b9aece4f7673bbf22435faa2
Ankush <ankushpndt@gmail.com>
2022-04-03 00:15:57 +0530

    refactor: removed console.log

--- src/checkItem.js
+++ src/checkItem.js
@@ -1,4 +1,3 @@
 export const checkItem = (array, _id) => {
-    console.log(!!array?.find((item) => item?.productId?._id === _id));
     return !!array?.find((item) => item?.productId?._id === _id);
 };
